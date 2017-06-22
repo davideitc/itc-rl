c85db0306074a775e64c3ca0b4f4b3414ce20f41
aess project <aess@taess.regione.lombardia.it>
2017-06-22 11:46:03 +0200

    prova sovrapposizione modifiche

--- GitServer.php
+++ GitServer.php
@@ -1,3 +1,6 @@
 <?php
 echo "la comunicazione tra la cartella su github e la cartella sul server funziona!";
+echo "modifica 1: 11:05 sul file preso dalla cartella sul server";
+
+echo "Modifica 2: 11:44 dopo modifica su cartella";
 ?>
