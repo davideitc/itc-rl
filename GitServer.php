c02ea2a6bb25c60b6de114c7b02eba2ea2b8b12c
cateItc <colombo@itc.cnr.it>
2017-06-22 11:41:12 +0200

    modifica file cartella

--- GitServer.php
+++ GitServer.php
@@ -1,3 +1,5 @@
 <?php
 echo "la comunicazione tra la cartella su github e la cartella sul server funziona!";
+
+echo "Modifico direttamente nella cartella di lavoro: controllo funzionamento sovrapposizioni modifiche";
 ?>
