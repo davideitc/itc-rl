664ece453cc8f140f3688c405e54540d9628d47e
cateItc <colombo@itc.cnr.it>
2017-06-23 11:23:13 +0200

    sistemo i due file fusi

--- testCate.php
+++ testCate.php
@@ -3,9 +3,6 @@ echo "Hello world!!!";
 echo "Hello git!";
 echo "prova prova prova";
 echo "scrivo pippo alle 16:20";
-<<<<<<< HEAD
 echo "scritto nella cartella locale alle 11:16";
-=======
 echo "scritto sul server alle 10:41";
->>>>>>> fe1088aafce13cb622adbfa9f18721c040a7e698
 ?>
