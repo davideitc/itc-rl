963d306ffced47ccc203140f861f9aff8af6bb15
aess project <aess@taess.regione.lombardia.it>
2017-06-23 10:10:18 +0200

    aggiunto commento accanto a head

--- GitServer.php
+++ GitServer.php
@@ -1,6 +1,6 @@
-<?php
+b <?php
 echo "la comunicazione tra la cartella su github e la cartella sul server funziona!";
-<<<<<<< HEAD
+<<<<<<< HEAD  echo "dopo il commit sono comparse anche le scritte tecniche presenti in git diff";
 echo "modifica 1: 11:05 sul file preso dalla cartella sul server";
 echo "Modifico direttamente nella cartella di lavoro: controllo funzionamento sovrapposizioni modifiche";
 echo "Modifica 2: 11:44 dopo modifica su cartella";
